8d149d6fcad51cc8ab9ddf209bb635ffd71d0417
Ponte, Clayton C <ccponte@gmail.com>
2022-04-13 18:53:14 -0300

    Initial commit

---
+++ src/main/java/com/ccponte/fwc22/FifaWorldCup2022Application.java
@@ -0,0 +1,13 @@
+package com.ccponte.fwc22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaWorldCup2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaWorldCup2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccponte/fwc22/FifaWorldCup2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccponte.fwc22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaWorldCup2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
